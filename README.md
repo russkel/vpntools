b7c38632e600368e3982e94b0894b56689921ca1
Russ Webber <russ@rw.id.au>
2017-05-12 21:45:18 +1000

    changed to tough configuration, added templating instead of crude replacing

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 Simple scripts to assist VPN usage
 
 ## update_pia.py
-Downloads the OpenVPN configuration files from PIA and modifies them to suit.
+Downloads the OpenVPN configuration files from PIA and extracts the hostnames and ports. It then creates configuration files based on a provided template (derived from the TCP tough PIA configuration) incorporating an auth file and openresolv support.
+
+You should put your credentials in pia/pia-auth.conf in the OpenVPN client configuration folder, which should be suitably restricted.
 
 To use:
 ```sh
   pacman -S openvpn openresolv
   mkdir confs
-  python3 update_pia.py
-  sudo systemctl start openvpn@Russia
+  python3 update_pia.py --install
+  sudo systemctl start openvpn@Brazil
 ```
--- update_pia.py
+++ update_pia.py
@@ -2,7 +2,16 @@ import os
 import sys
 import subprocess
 import zipfile
+import argparse
+import json
+from string import Template
 
+parser = argparse.ArgumentParser(description='Obtain PIA VPNs and write config files.')
+parser.add_argument('--refresh', action='store_true', default=False,
+                    help='Write the config files ignoring checks for new file.')
+parser.add_argument('--install', action='store_true', default=False,
+                    help='Install the configuration files into the system.')
+args = parser.parse_args()
 
 def mod_time():
     try:
@@ -12,20 +21,17 @@ def mod_time():
         return None
 
 
-def fix_conf(ovpn):
-    for i, line in enumerate(ovpn):
-        if not line.strip():
-            ovpn[i] = ''
-        if line.startswith("auth-user-pass"):
-            ovpn[i] = "auth-user-pass pia/pia-auth.conf\n"
+def extract_uri(ovpn):
+    for line in ovpn:
+        if line.startswith("remote "):
+            return line.strip().split()[1:3]
 
-    ovpn += ["script-security 2\n", "up /etc/openvpn/update-resolv-conf.sh\n", "down /etc/openvpn/update-resolv-conf.sh\n"]
-    return ovpn
+    return (None, None)
 
 modtime = mod_time()
-subprocess.run("curl -z openvpn.zip https://www.privateinternetaccess.com/openvpn/openvpn.zip -o openvpn.zip", shell=True)
+subprocess.run("curl -z openvpn.zip https://www.privateinternetaccess.com/openvpn/openvpn-strong-tcp.zip -o openvpn.zip", shell=True)
 
-if modtime == mod_time():
+if modtime == mod_time() and not args.refresh:
     # file is intact, do nothing
     print("No new files.")
     sys.exit()
@@ -33,17 +39,34 @@ if modtime == mod_time():
 subprocess.run("mkdir -p confs", shell=True)
 subprocess.run("rm -f confs/*", shell=True)
 
+servers = {}
+
 with zipfile.ZipFile("openvpn.zip") as zf:
     for fn in zf.namelist():
         if fn.endswith(".ovpn"):
-            new_fn = "confs/{}".format(fn.replace(' ', '_').replace('.ovpn', '.conf'))
-            print("Writing {}".format(new_fn))
+            name, ext = os.path.splitext(fn)
 
-            with zf.open(fn, mode="r") as zff, open(new_fn, "w") as out:
+            with zf.open(fn, mode="r") as zff:
                 ovpn = [b.decode() for b in zff.readlines()]
-                out.writelines(fix_conf(ovpn))
+
+            hostname, port = extract_uri(ovpn)
+            servers[name] = (hostname, port)
+
         elif fn.endswith('.crt') or fn.endswith('.pem'):
             zf.extract(fn, "confs/")
 
-print("Installing configuration files")
-subprocess.run("sudo cp confs/* /etc/openvpn/clients/", shell=True)
+with open('pia_hosts.json', "w") as out:
+    json.dump(servers, out)
+
+conf_file = Template("".join(open("template.conf", "r").readlines()))
+
+for name, (hostname, port) in servers.items():
+    new_fn = "confs/{}.conf".format(name.replace(' ', '_'))
+    print("Writing {}".format(new_fn))
+
+    with open(new_fn, "w") as out:
+        out.write(conf_file.substitute(hostname=hostname, port=port))
+
+if args.install:
+    print("Installing configuration files")
+    subprocess.run("sudo cp confs/* /etc/openvpn/clients/", shell=True)
